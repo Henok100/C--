c33b39e0112b07b2a7c5da4be15634d2d32f57ef
Henok100 <henokgashaw15@gmail.com>
2023-02-02 13:58:59 +0300

    Day_1

---
+++ Part_1_The_Basics/Lesson_1_GettingStarted/exe1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    int x = 8;
+    int y = 9;
+    std::cout << std::endl;
+    std::cout << x - y << " " << x * y << " " << x + y;
+    std::cout << std::endl;
+
+    return 0;
+}
